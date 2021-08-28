42f54ed3e9d73042d440de19afceb3f996682396
M F M Fazrin <mfmfazrin1986@gmail.com>
2021-08-28 10:51:28 +0530

    Add project files.

---
+++ Data/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Weather.Data
+{
+    public class Weather
+    {
+        public int Humidity { get; set; }
+        public int Temperature { get; set; }
+        public int Min_Temperature { get; set; }
+        public int Max_Temperature { get; set; }
+    }
+}
